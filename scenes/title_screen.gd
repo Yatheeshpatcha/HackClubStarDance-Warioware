2963726698d3291dbdf2fbccb2977a5b518c66f3
Yatheeshpatcha <yatheeshpatcha@gmail.com>
2026-06-25 10:34:09 -0600

    created menu and programed buttons

---
+++ scenes/title_screen.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_start_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://level_scene.tscn")
+
+	pass # Replace with function body.
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
+
+
+func _on_setting_pressed() -> void:
+	get_tree().change_scene_to_file("res://settings.tscn")
