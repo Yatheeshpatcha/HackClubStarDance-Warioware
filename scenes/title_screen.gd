40157bfcd70fab3571ec1c48c5e9dfc00b5637e8
Yatheeshpatcha <yatheeshpatcha@gmail.com>
2026-06-26 17:54:17 -0600

    coded a camera to follow player and created platforms and suns

--- scenes/title_screen.gd
+++ scenes/title_screen.gd
@@ -12,7 +12,7 @@ func _process(delta: float) -> void:
 
 
 func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://level_scene.tscn")
+	get_tree().change_scene_to_file("res://minigame_1.tscn")
 
 	pass # Replace with function body.
 
